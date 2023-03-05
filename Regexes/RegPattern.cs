70ea41921464f42f2d3e4be0f3e8538b17712775
Anton <anton260903@gmail.com>
2023-03-06 00:39:07 +0200

    email pattern added

---
+++ Regexes/RegPattern.cs
@@ -0,0 +1,10 @@
+﻿namespace Regexes
+{
+    public static class RegPattern
+    {
+        public const string Email =
+            @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
+            + "@"
+            + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
+    }
+}
